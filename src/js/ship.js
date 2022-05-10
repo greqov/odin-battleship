eeac293af5445cd4ba886d54cd984db6470f95b2
Ivan Greqov <idgreq@bk.ru>
2022-05-10 12:20:20 +0300

    Ship: add factory function, add tests for length/hits

--- src/js/ship.js
+++ src/js/ship.js
@@ -1,5 +1,23 @@
-function shipFactory() {
-  return {};
+function shipFactory(size) {
+  const body = new Array(size).fill(0);
+
+  const ship = {
+    hit(n) {
+      if (n >= 0 && n < body.length) {
+        body[n] = 1;
+      }
+    },
+
+    get body() {
+      return body;
+    },
+
+    get length() {
+      return body.length;
+    },
+  };
+
+  return ship;
 }
 
 export default shipFactory;
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -3,3 +3,24 @@ import shipFactory from '../src/js/ship';
 test('happy test', () => {
   expect(typeof shipFactory).toBe('function');
 });
+
+test('ship has some size', () => {
+  const ship = shipFactory(3);
+  expect(ship.length).toBe(3);
+});
+
+test('ship can register hits', () => {
+  // NOTE: not sure about using 'body'
+  const ship = shipFactory(3);
+  ship.hit(1);
+  ship.hit(2);
+  expect(ship.body).toEqual([0, 1, 1]);
+});
+
+test('hits outside of ship dont affect it', () => {
+  const ship = shipFactory(2);
+  ship.hit(4);
+  // NOTE: does it make sense to test negative index?
+  ship.hit(-1);
+  expect(ship.body).toEqual([0, 0]);
+});
